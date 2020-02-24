5fb794980c4e164e8c5f620d25972ee701b8255a
Andrew Lapshov <titan1um@yandex.ru>
2020-02-24 20:54:08 +0300

    pre-sprint-15

--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
-    message,
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
 
   next();
